7fb49f253919c992d37592e1ef51b0a95841a3bd
Hawkeye <swarup@agpaytech.co.uk>
2022-05-17 20:18:18 +0530

    migration added

---
+++ database/migrations/2022_05_17_143259_create_currencies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id();
+            $table->string('iso_code');
+            $table->string('iso_numeric_code')->nullable();
+            $table->string('common_name');
+            $table->string('official_name');
+            $table->string('symbol');
+            $table->timestamp('created');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
